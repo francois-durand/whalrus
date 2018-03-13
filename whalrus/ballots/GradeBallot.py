9b23012db58722db8ac13b9b7ff37c2cd67d3b90
yannche <yann.che@gmail.com>
2018-03-13 16:00:21 +0100

    imports

--- whalrus/ballots/GradeBallot.py
+++ whalrus/ballots/GradeBallot.py
@@ -19,6 +19,7 @@ This file is part of Whalrus.
     You should have received a copy of the GNU General Public License
     along with Whalrus.  If not, see <http://www.gnu.org/licenses/>.
 """
+from whalrus.ballots.NumericBallot import NumericBallot
 
 
 class GradeBallot(NumericBallot):
