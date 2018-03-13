624669fbb4aab7f0c5a7c9aeac2865d317e3e26e
Francois Durand <fradurand@gmail.com>
2018-03-13 14:30:35 +0100

    Examples of class files

---
+++ docs/reference.rst
@@ -0,0 +1,18 @@
+=========
+Reference
+=========
+
+A Nice Section
+--------------
+
+.. autoclass:: whalrus.MyClass1
+    :members:
+
+Another Nice Section
+--------------------
+
+.. autoclass:: whalrus.MyClass2
+    :members:
+
+.. autoclass:: whalrus.MyClass3
+    :members:
--- whalrus/__init__.py
+++ whalrus/__init__.py
@@ -2,6 +2,10 @@
 
 """Top-level package for Whalrus."""
 
-__author__ = """François Durand"""
+__author__ = """Sylvain Bouveret, Yann Chevaleyre and François Durand"""
 __email__ = 'fradurand@gmail.com'
 __version__ = '0.1.0'
+
+from .SubPackage1.MyClass1 import MyClass1
+from .SubPackage2.MyClass2 import MyClass2
+from .SubPackage2.MyClass3 import MyClass3
