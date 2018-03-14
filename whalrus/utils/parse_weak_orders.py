db5e1ad80b1a1345fd9c4964457fdab90f47a95d
yannche <yann.che@gmail.com>
2018-03-14 18:49:35 +0100

    - added exception example in doctest - added parseAll (this forces the string to be parsed from start to end)

--- whalrus/utils/parse_weak_orders.py
+++ whalrus/utils/parse_weak_orders.py
@@ -1,9 +1,10 @@
-from pyparsing import Group, Word, Or, ZeroOrMore, OneOrMore, alphas, nums
+from pyparsing import Group, Word, ZeroOrMore, alphas, nums, ParseException
 
 
 def parse_weak_order(s):
     """
     Converts a string representing a weak-order to a dictionary
+    Throws a 'ParseException' if the string cannot be parsed
 
     Example:
 
@@ -11,17 +12,26 @@ def parse_weak_order(s):
     >>> parse_weak_order(s)
     {'Jean': 3, 'Titi': 3, 'tata32': 3, 'moi': 2, 'toi': 1, 'nous': 1, 'eux': 0}
 
+    Other example, in which an exception is thrown because the string is incorrect:
+
+    >>> try:
+    ...    parse_weak_order('a * b')
+    ... except ParseException:
+    ...    print('Failed')
+    Failed
+
     """
+
     candidate = Word(alphas.upper() + alphas.lower() + nums + '_')
     equiv_class = Group(candidate + ZeroOrMore(Word('~').suppress() + candidate))
-    weakpref = equiv_class + OneOrMore(Word('>').suppress() + equiv_class)
+    weakpref = equiv_class + ZeroOrMore(Word('>').suppress() + equiv_class)
 
-    parsed = weakpref.parseString(s).asList()
-    l = []
+    parsed = weakpref.parseString(s, parseAll=True).asList()
+    lst = []
 
     for v, t in enumerate(reversed(parsed)):
-        l = [(c, v) for c in t] + l
-    return dict(l)
+        lst = [(c, v) for c in t] + lst
+    return dict(lst)
 
 
 if __name__ == '__main__':
