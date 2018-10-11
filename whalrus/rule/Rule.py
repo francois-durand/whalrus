46b18097dbc1368b2917aca6d3459a42605467e8
francois-durand <fradurandpub@gmail.com>
2018-10-11 16:33:33 +0200

    Minor edit in doc

--- whalrus/rule/Rule.py
+++ whalrus/rule/Rule.py
@@ -8,34 +8,34 @@ from typing import Union
 
 
 class Rule(DeleteCacheMixin):
+    """
+    A voting rule.
+
+    :param ballots: if mentioned, will be passed to `__call__` immediately after initialization.
+    :param weights: if mentioned, will be passed to `__call__` immediately after initialization.
+    :param voters: if mentioned, will be passed to `__call__` immediately after initialization.
+    :param candidates: if mentioned, will be passed to `__call__` immediately after initialization.
+    :param converter: if mentioned, will be passed to `__call__` immediately after initialization.
+    :param tie_break: a tie-break rule.
+    :param default_converter: the default converter that is used to convert input ballots. This converter is
+        used when no converter is explicitly given to `__call__`.
+
+    A :class:`Rule` object is a callable whose inputs are ballots and optionally weights, voters, candidates and a
+    converter. When the rule is called, it loads the profile. The output of the call is the rule itself. But
+    after the call, you can access to the computed variables (ending with an underscore), such as
+    :attr:`cowinners_`.
+
+    At the initialization of a :class:`Rule` object, some options can be given, such as a tie-break rule and a
+    default converter. In some subclasses, there can also be an option about the way to count abstentions, etc.
+
+    Cf. :class:`RulePlurality` for some examples.
+
+    Remark: this `__init__` must always be called at the end of the subclasses' `__init__`.
+    """
 
     def __init__(self, ballots: Union[list, Profile]=None, weights: list=None, voters: list=None,
                  candidates: set=None, converter: ConverterBallot=None,
                  tie_break: Priority=Priority.UNAMBIGUOUS, default_converter: ConverterBallot=ConverterBallotGeneral()):
-        """
-        A voting rule.
-
-        :param ballots: if mentioned, will be passed to `__call__` immediately after initialization.
-        :param weights: if mentioned, will be passed to `__call__` immediately after initialization.
-        :param voters: if mentioned, will be passed to `__call__` immediately after initialization.
-        :param candidates: if mentioned, will be passed to `__call__` immediately after initialization.
-        :param converter: if mentioned, will be passed to `__call__` immediately after initialization.
-        :param tie_break: a tie-break rule.
-        :param default_converter: the default converter that is used to convert input ballots. This converter is
-            used when no converter is explicitly given to `__call__`.
-
-        A :class:`Rule` object is a callable whose inputs are ballots and optionally weights, voters, candidates and a
-        converter. When the rule is called, it loads the profile. The output of the call is the rule itself. But
-        after the call, you can access to the computed variables (ending with an underscore), such as
-        :attr:`cowinners_`.
-
-        At the initialization of a :class:`Rule` object, some options can be given, such as a tie-break rule and a
-        default converter. In some subclasses, there can also be an option about the way to count abstentions, etc.
-
-        Cf. :class:`RulePlurality` for some examples.
-
-        Remark: this `__init__` must always be called at the end of the subclasses' `__init__`.
-        """
         # Parameters
         self.tie_break = tie_break
         self.default_converter = default_converter
