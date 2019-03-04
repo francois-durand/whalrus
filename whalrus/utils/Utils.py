15b1345865a8a9b3c8d8e2848c1bff634d4cf80f
francois-durand <fradurandpub@gmail.com>
2019-03-04 16:27:57 +0100

    Rewrite cached_property and DeleteCacheMixin
    With the old version, the doctests located in a cached_property were not run by pytest when launching all tests. The reason was that cached_properties were class instances. Now they are real properties, so it works! By the way, the new version is simpler.

--- whalrus/utils/Utils.py
+++ whalrus/utils/Utils.py
@@ -22,61 +22,50 @@ from pyparsing import Group, Word, ZeroOrMore, alphas, nums, ParseException
 from bisect import bisect_left
 
 
-# noinspection PyPep8Naming
-class cached_property:
+def _cache(f):
+    """
+    Auxiliary decorator used by ``cached_property``.
+
+    :param f: a method with no argument (except ``self``).
+    :return: the same function, but with a `caching' behavior.
+    """
+    name = f.__name__
+
+    # noinspection PyProtectedMember
+    def _f(*args):
+        try:
+            return args[0]._cached_properties[name]
+        except KeyError:
+            # Not stored in cache
+            value = f(*args)
+            args[0]._cached_properties[name] = value
+            return value
+        except AttributeError:
+            # cache does not even exist
+            value = f(*args)
+            args[0]._cached_properties = {name: value}
+            return value
+    _f.__doc__ = f.__doc__
+    return _f
+
+
+def cached_property(f):
     """
     Decorator used in replacement of @property to put the value in cache automatically.
 
     The first time the attribute is used, it is computed on-demand and put in cache. Later accesses to the
     attributes will use the cached value.
 
-    Technically, this is a "descriptor".
-
-    Adapted from https://stackoverflow.com/questions/4037481/caching-attributes-of-classes-in-python.
-
     Cf. :class:`DeleteCacheMixin` for an example.
     """
-
-    def __init__(self, factory: callable):
-        """
-        This code runs when the decorator is applied to the function (i.e. when the function is defined).
-
-        :meth:`factory` is the function.
-        """
-        self._factory = factory
-        self._attr_name = factory.__name__
-
-    def __get__(self, instance: object, owner: object) -> object:
-        """
-        This code runs only when the decorated function is directly called (which happens only when the value is not
-        in cache).
-        """
-        # This hack is used so that the decorated function has the same docstring as the original function.
-        if instance is None:
-            def f():
-                pass
-            f.__doc__ = self._factory.__doc__
-            f = property(f)
-            return f
-        # Compute the value.
-        value = self._factory(instance)
-        # Create the attribute and assign the value.
-        # In the Python precedence order, the attribute "hides" the function of the same name.
-        setattr(instance, self._attr_name, value)
-        # Add the attribute name in the set cached_properties of the instance.
-        try:
-            instance.cached_properties.add(self._attr_name)
-        except AttributeError:
-            instance.cached_properties = {self._attr_name}
-        # Return the value.
-        return value
+    return property(_cache(f))
 
 
 class DeleteCacheMixin:
     """
     Mixin used to delete cached properties.
 
-    Cf. decorator :class:`cached_property`.
+    Cf. decorator :meth:`cached_property`.
 
     >>> class Example(DeleteCacheMixin):
     ...     @cached_property
@@ -94,13 +83,10 @@ class DeleteCacheMixin:
     Big computation...
     42
     """
+
+    # noinspection PyAttributeOutsideInit
     def delete_cache(self) -> None:
-        if not hasattr(self, 'cached_properties'):
-            return
-        for p in self.cached_properties:
-            del self.__dict__[p]
-        # noinspection PyAttributeOutsideInit
-        self.cached_properties = set()
+        self._cached_properties = dict()
 
 
 def parse_weak_order(s: str) -> list:
