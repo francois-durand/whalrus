c3d1ab62e5e199269e4671c7a1ecf3e9fff78c8b
francois-durand <fradurandpub@gmail.com>
2020-11-26 11:18:16 +0100

    Migrate the Package Helper 2, continued
    * Prepare interface with Codecov.
    * Prepare support for Numpy documentation style.
    * Prepare support for notebooks in documentation.
    * Remove version numbers for requirements_dev.
    * Remove support for Python <3.6, add support for Python 3.8.

--- setup.py
+++ setup.py
@@ -26,9 +26,9 @@ setup(
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
     description="Which Alternative Represents Us, a package for voting rules",
     entry_points={
