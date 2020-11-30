6bd1dfa9e5a0d86b672b85e529652925312a0e74
francois-durand <fradurandpub@gmail.com>
2020-11-30 13:35:13 +0100

    Update test_ScorerBucklin.py

--- tests/test_ScorerBucklin.py
+++ tests/test_ScorerBucklin.py
@@ -6,6 +6,6 @@ def test():
         >>> scorer = ScorerBucklin(k=3, absent_receive_points=False)
         >>> ballot = BallotOrder('a > b')
         >>> scorer(ballot, candidates={'a', 'b', 'c', 'd'}).scores_as_floats_
-         {'a': 1.0, 'b': 1.0, 'c': 0.0, 'd': 0.0}
+        {'a': 1.0, 'b': 1.0, 'c': 0.0, 'd': 0.0}
     """
     pass
